be4b96be8f0ffadc2f5d06e62bbadb5934064d8a
Scott Ries <scottmries@gmail.com>
2017-11-29 23:19:37 -0500

    Add next element getter.

--- node-server/scripts/markov-chain.js
+++ node-server/scripts/markov-chain.js
@@ -2,24 +2,59 @@ class MarkovChain {
   constructor(order, elements) {
     this.order = order
     this.element = elements
-    this.generateMarkovStructure()
+    this.lastRow = new Array()
+    this.generateMarkovMatrix()
     this.generateProbabilities()
   }
 
-  generateMarkovStructure() {
-    this.markovStructure = this.generateStructureLevel(this.elements, this.order, this.getRandomElementProbability)
+  generateMarkovMatrix() {
+    this.matrix = this.generateStructureLevel(this.elements, this.order, this.getRandomElementProbabilities(this.elements))
   }
 
   generateStructureLevel(elements, n, callback) {
     let obj = {}
-    elements.map((el) => obj[el] = n === 0 ? callback().bind(this) : this.generateStructureLevel(elements, n - 1))
+    elements.map((el) => obj[el] = n === 1 ? callback().bind(this) : this.generateStructureLevel(elements, n - 1))
     return obj
   }
 
-  getRandomElementProbability() {
-    this.remainingChance = typeof this.remainingChange === "undefined" || this.remainingChance <= 0 ? 1 : this.remainingChance
-    const probability = Math.random() * this.remainingChance
-    this.remainingChance = remainingChance - probability
-    return probability
+  getRandomElementProbabilities(elements) {
+    let elementProbabilities = []
+    let remainingChance = 1
+    let remainingElements = elements
+    let baseProbability = 0
+    elements.map(() => {
+      let index = parseInt(Math.random() * remainingElements.length)
+      let probability = Math.random() * this.remainingChance
+      elementProbabilities.push({
+        exclusiveMaximum: baseProbability + probability,
+        inclusiveMinimum: baseProbability,
+        element: remainingElements[index]
+      })
+      remainingElements = remainingElements.slice(0, index).slice(index)
+    })
+  }
+
+  traverseObject(object, keys) {
+    keys.forEach((key) => {
+      if (!typeof object[key] === "undefined") {
+        object = object[key]
+      } else {
+        return undefined
+      }
+    }!(typeof object[key] === "undefined") ? object = object[key] : undefined)
+  }
+
+  get next() {
+    if (this.lastRow.length < this.order) {
+      this.lastRow.push(this.elements[parseInt(Math.random() * (this.elements.length - 1))])
+    } else {
+      let object = this.matrix
+      this.lastRow.map((el) => object = object[el])
+      const throw = Math.random()
+      // object should be an array at this point
+      const {element: next} = object.filter((el) => throw >= el.includeMinimum && < el.exclusiveMaximum)
+      // remove the array's first element and append the next one to the end
+      this.elements.shift().push(next)
+    }
   }
 }
